a485a28c89dd53b062b23a423f9dedb08493e4d2
Artem Wolkov <wolkov.art.leo@yandex.ru>
2022-07-26 22:01:22 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect} from 'react';
 import './App.css';
+import {useAppDispatch, useAppSelector} from "./hooks/redux";
+import {fetchUsers} from "./store/reducers/ActionCreators";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const dispatch = useAppDispatch()
+    const {users, isLoading, error} = useAppSelector(state => state.userReducer)
+
+    useEffect(() => {
+        dispatch(fetchUsers())
+    }, [])
+
+    return (
+        <div className="App">
+            {isLoading && <h1>Идет загрузка...</h1>}
+            {error && <h1>error</h1>}
+            {JSON.stringify(users, null, 2)}
+        </div>
+    );
 }
 
 export default App;
---
+++ src/store/reducers/ActionCreators.ts
@@ -0,0 +1,27 @@
+import {AppDispatch} from "../store";
+import axios from "axios";
+import {IUser} from "../../models/IUser";
+import {userSlice} from "./userSlice";
+import {createAsyncThunk} from "@reduxjs/toolkit";
+
+// export const fetchUsers = () => async (dispatch: AppDispatch) => {
+//   try {
+//     dispatch(userSlice.actions.usersFetching())
+//     const response = await axios.get<IUser[]>('https://jsonplaceholder.typicode.com/users')
+//     dispatch(userSlice.actions.usersFetchingSuccess(response.data))
+//   } catch (e) {
+//     dispatch(userSlice.actions.usersFetchingError('Ошибка'))
+//   }
+// }
+
+export const fetchUsers = createAsyncThunk(
+    'user/fetchAll',
+    async (_, thunkAPI) => {
+      try {
+        const response = await axios.get<IUser[]>('https://jsonplaceholder.typicode.com/users')
+        return response.data
+      } catch (e) {
+        return thunkAPI.rejectWithValue('Не удалось загрузить пользователей')
+      }
+    }
+)
