0618f363ac97a05a23d3f45e9b7ba79b0667a260
Artem Wolkov <wolkov.art.leo@yandex.ru>
2022-08-02 07:08:11 +0300

    Posts

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,18 @@ import {useAppDispatch, useAppSelector} from "./hooks/redux";
 import {fetchUsers} from "./store/reducers/ActionCreators";
 
 function App() {
-    const dispatch = useAppDispatch()
-    const {users, isLoading, error} = useAppSelector(state => state.userReducer)
-
-    useEffect(() => {
-        dispatch(fetchUsers())
-    }, [])
+    // const dispatch = useAppDispatch()
+    // const {users, isLoading, error} = useAppSelector(state => state.userReducer)
+    //
+    // useEffect(() => {
+    //     dispatch(fetchUsers())
+    // }, [])
 
     return (
         <div className="App">
-            {isLoading && <h1>Идет загрузка...</h1>}
-            {error && <h1>error</h1>}
-            {JSON.stringify(users, null, 2)}
+            {/*{isLoading && <h1>Идет загрузка...</h1>}*/}
+            {/*{error && <h1>error</h1>}*/}
+            {/*{JSON.stringify(users, null, 2)}*/}
         </div>
     );
 }
